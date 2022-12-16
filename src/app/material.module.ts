4dc1d47e9c12d67fc97851c4caa532d2b73b36ad
WemersonWalcley <wemerson.walcley@outlook.com>
2022-12-15 21:33:13 -0300

    Adding check-box and date-picker required validation

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,9 +3,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
-    imports:[MatButtonModule, MatIconModule, MatInputModule, MatDatepickerModule],
-    exports: [MatButtonModule, MatIconModule, MatInputModule, MatDatepickerModule]
+    imports:[MatButtonModule, MatIconModule, MatInputModule, MatDatepickerModule, MatCheckboxModule],
+    exports: [MatButtonModule, MatIconModule, MatInputModule, MatDatepickerModule, MatCheckboxModule]
 })
 export class MaterialModule{}
